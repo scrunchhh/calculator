a332e9bc644eeb747725fcdd5ab1ddfba48bd33e
scrunchhh <119765244+scrunchhh@users.noreply.github.com>
2022-12-19 18:47:49 -0500

    Added negatives, powers, and decimals

--- script.js
+++ script.js
@@ -9,22 +9,30 @@ function onClick(e) {
     && e.target.textContent != ' / ' 
     && e.target.textContent != ' * ' 
     && e.target.textContent != ' + ' 
+    && e.target.textContent != ' ^ ' 
     && e.target.textContent != ' - ') {
         calcList = ''
     }
-    //add input to the string
-    calcList += e.target.textContent
-    console.log(calcList)
     //on equals click, run the calculator on the string input
     if (e.target.textContent == ' =') {
+        console.log(calcList)
         calcSplit = calcList.split(' ')
-        calcSplit.pop()
         console.log(calcSplit)
         //run code until one final number remains
         while (calcSplit.length != 1) {
-            if (calcSplit.includes('/')) {
+            if (calcSplit.includes('^')) {
                 //operate on both numbers around an operator, then remove them
                 //leaving only the result
+                for (let i = 0; i <= calcSplit.length; i++) {
+                    if (calcSplit[i] == '^') {
+                        calcSplit[i] = (+calcSplit[i - 1]) ** +calcSplit[i + 1]
+                        calcSplit.splice(i - 1, 1)
+                        calcSplit.splice(i, 1)
+                        console.log(calcSplit)
+                    }
+                }
+            }
+            if (calcSplit.includes('/')) {
                 for (let i = 0; i <= calcSplit.length; i++) {
                     if (calcSplit[i] == '/') {
                         calcSplit[i] = +calcSplit[i - 1] / +calcSplit[i + 1]
@@ -64,18 +72,32 @@ function onClick(e) {
                     }
                 }
             }
-            
         }
         calcList = calcSplit[0]
         console.log(calcList)
     } else if (e.target.textContent == 'C') {
         calcList = ''
+    } else if (e.target.textContent == 'NEG') {
+        calcList += '-'
+    } else if (e.target.textContent == 'DEL') {
+        if (calcList[calcList.length - 1] == ' ') {
+            calcList = calcList.slice(0, -3)
+            console.log(calcList)
+        } else {
+            calcList = calcList.slice(0, -1)
+            console.log(calcList)
+        }
+        
+    } else {
+        //add input to the string
+        calcList += e.target.textContent
+        console.log(calcList)
     }
 }
 
 //add digits to digitBox
-for (let i = 0; i <= 11; i++) {
-    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'C', 0, " ="]
+for (let i = 0; i <= 15; i++) {
+    let arr = [1, 2, 3, '.', 4, 5, 6, ' ^ ', 7, 8, 9, 'NEG', 'C', 0, " =", 'DEL']
     let digit = document.createElement('button')
     digit.classList.add('digit')
     digit.textContent = arr[i]
