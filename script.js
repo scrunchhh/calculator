c9c815735b22eb31710933439e5f5747928d923e
scrunchhh <119765244+scrunchhh@users.noreply.github.com>
2022-12-19 19:04:28 -0500

    changed capitals to lowercase on neg and del

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const digitsBox = document.querySelector('.digits')
 const outputBox = document.querySelector('.output')
 
 function onClick(e) {
-    console.log(typeof(calcList))
     //clear string on click after getting result unless its an operator
     if (typeof(calcList) == 'number' 
     && e.target.textContent != ' / ' 
@@ -15,9 +14,7 @@ function onClick(e) {
     }
     //on equals click, run the calculator on the string input
     if (e.target.textContent == ' =') {
-        console.log(calcList)
         calcSplit = calcList.split(' ')
-        console.log(calcSplit)
         //run code until one final number remains
         while (calcSplit.length != 1) {
             if (calcSplit.includes('^')) {
@@ -28,7 +25,6 @@ function onClick(e) {
                         calcSplit[i] = (+calcSplit[i - 1]) ** +calcSplit[i + 1]
                         calcSplit.splice(i - 1, 1)
                         calcSplit.splice(i, 1)
-                        console.log(calcSplit)
                     }
                 }
             }
@@ -38,7 +34,6 @@ function onClick(e) {
                         calcSplit[i] = +calcSplit[i - 1] / +calcSplit[i + 1]
                         calcSplit.splice(i - 1, 1)
                         calcSplit.splice(i, 1)
-                        console.log(calcSplit)
                     }
                 }
             }
@@ -48,7 +43,6 @@ function onClick(e) {
                         calcSplit[i] = +calcSplit[i - 1] * +calcSplit[i + 1]
                         calcSplit.splice(i - 1, 1)
                         calcSplit.splice(i, 1)
-                        console.log(calcSplit)
                     }
                 }
             }
@@ -58,7 +52,6 @@ function onClick(e) {
                         calcSplit[i] = +calcSplit[i - 1] + +calcSplit[i + 1]
                         calcSplit.splice(i - 1, 1)
                         calcSplit.splice(i, 1)
-                        console.log(calcSplit)
                     }
                 }
             }
@@ -68,36 +61,32 @@ function onClick(e) {
                         calcSplit[i] = +calcSplit[i - 1] - +calcSplit[i + 1]
                         calcSplit.splice(i - 1, 1)
                         calcSplit.splice(i, 1)
-                        console.log(calcSplit)
                     }
                 }
             }
         }
         calcList = calcSplit[0]
-        console.log(calcList)
     } else if (e.target.textContent == 'C') {
         calcList = ''
-    } else if (e.target.textContent == 'NEG') {
+    } else if (e.target.textContent == 'neg') {
         calcList += '-'
-    } else if (e.target.textContent == 'DEL') {
+    } else if (e.target.textContent == 'del') {
         if (calcList[calcList.length - 1] == ' ') {
             calcList = calcList.slice(0, -3)
-            console.log(calcList)
         } else {
             calcList = calcList.slice(0, -1)
-            console.log(calcList)
         }
         
     } else {
         //add input to the string
         calcList += e.target.textContent
-        console.log(calcList)
     }
+    outputBox.textContent = `${calcList}`
 }
 
 //add digits to digitBox
 for (let i = 0; i <= 15; i++) {
-    let arr = [1, 2, 3, '.', 4, 5, 6, ' ^ ', 7, 8, 9, 'NEG', 'C', 0, " =", 'DEL']
+    let arr = [1, 2, 3, '.', 4, 5, 6, ' ^ ', 7, 8, 9, 'neg', 'C', 0, " =", 'del']
     let digit = document.createElement('button')
     digit.classList.add('digit')
     digit.textContent = arr[i]
